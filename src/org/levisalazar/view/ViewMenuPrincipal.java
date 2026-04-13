a3a4abe746adc29aeb7915e9a0f5930ea0f3bfc3
levi-Salazar432 <lsalazar-2026039@kinal.edu.gt>
2026-04-12 19:59:24 -0600

    005 Se agrego el menu de matematicas

--- src/org/levisalazar/view/ViewMenuPrincipal.java
+++ src/org/levisalazar/view/ViewMenuPrincipal.java
@@ -1,5 +1,6 @@
 
 package org.levisalazar.view;
+
 import java.util.Scanner;
  
 /**
@@ -11,7 +12,7 @@ public class ViewMenuPrincipal{
     public static void main(String[] args){
     Scanner sn = new Scanner(System.in);
     boolean salir = false; 
-    int opcionMenu = 0;
+    int opcionMenu =0; 
         do { 
             System.out.println("*********************************");
             System.out.println("Bienvenido Seleccione una opcion:");
@@ -25,9 +26,9 @@ public class ViewMenuPrincipal{
                 opcionMenu = Integer.parseInt(sn.nextLine()); 
                 
                 switch (opcionMenu) {
-                    case 1 -> MenuMatematicas(sn);
-                    case 2 -> MenuLogico(sn);
-                    case 3 -> MenuTexto(sn);
+                    case 1 ->MenuMatematicas(sn);
+                    case 2 ->MenuLogico(sn);
+                    case 3 ->MenuTexto(sn);
                     case 4 -> {
                         System.out.println("Saliendo del sistema!!");
                         salir = true; 
@@ -35,12 +36,34 @@ public class ViewMenuPrincipal{
                     default -> System.out.println("Opcion no valida");
                 } 
             } catch (NumberFormatException e){
-                System.out.println("Error numero invalido.");
+                System.out.println("Error numero invalido."  );
             }  
         } while (!salir);    
                 
-            }
-                
-        } 
+     }
+    
+        //---submenu de Matematicas---
+    boolean volver = false; 
+    do{
+        System.out.println("Caclculo De Operaciones Matimaticas");
+        System.out.println("1. Caclculadora de salario");
+        System.out.println("2. Área de un triangulo");
+        System.out.println("3. Promedio Academico");
+        System.out.println("4. Convertidor de Temperatura");
+        System.out.println("5. Perímetro de Círculo");
+        System.out.println("6. Volumen de un Cilindro");
+        System.out.println("7. Hipotenusa");
+        System.out.println("8. Días a Minutos");
+        System.out.println("9. Resto de Division");
+        System.out.println("10. Potencia al cubo");
+        System.out.println("11. Volver al Menú Principal");
+        System.out.print("Seleccione una operacion");
+        
+        int opcion = Integer.parseInt(sn.nextLine());
+        if(opcion == 11) volver = true;
+        else System.out.println("Ejecutando opcion" + opcion);
+    
+      } while(!volver);
+    }
     
 
