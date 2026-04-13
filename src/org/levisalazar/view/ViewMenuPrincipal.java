fc64556e776c49bfe5403369f17f065da4c8f16b
levi-Salazar432 <lsalazar-2026039@kinal.edu.gt>
2026-04-12 19:29:51 -0600

    004 Se agrego función al presionar salir

--- src/org/levisalazar/view/ViewMenuPrincipal.java
+++ src/org/levisalazar/view/ViewMenuPrincipal.java
@@ -4,24 +4,43 @@ import java.util.Scanner;
  
 /**
 *
-* @author miguel guzman 
+* @author levi Salazar 
 */
 public class ViewMenuPrincipal{
     //global
-    int opcionMenu =0;
-    Scanner leer = new Scanner (System.in);
-    public void mostrarMenu(){
-        while (opcionMenu == 0){
+    public static void main(String[] args){
+    Scanner sn = new Scanner(System.in);
+    boolean salir = false; 
+    int opcionMenu = 0;
+        do { 
+            System.out.println("*********************************");
             System.out.println("Bienvenido Seleccione una opcion:");
-            System.out.println("1.operaciones metematicas ");
-            System.out.println("2.operaciones de texto");
-            System.out.println("3.operaciones de logica ");
-            System.out.println("4.salir del sistema");
-            opcionMenu = Integer.parseInt(leer.nextLine());
-        }
-                }
-    public void mostrarMenuMatematicas(){
-        System.out.println("Menu de operaciones matematicas  ");
-        
-    }
-}
+            System.out.println("1.operaciones metematicas        ");
+            System.out.println("2.operaciones de texto           ");
+            System.out.println("3.operaciones de logica          ");
+            System.out.println("4.salir del sistema              ");
+            System.out.println("*********************************");
+          
+            try {
+                opcionMenu = Integer.parseInt(sn.nextLine()); 
+                
+                switch (opcionMenu) {
+                    case 1 -> MenuMatematicas(sn);
+                    case 2 -> MenuLogico(sn);
+                    case 3 -> MenuTexto(sn);
+                    case 4 -> {
+                        System.out.println("Saliendo del sistema!!");
+                        salir = true; 
+                    }
+                    default -> System.out.println("Opcion no valida");
+                } 
+            } catch (NumberFormatException e){
+                System.out.println("Error numero invalido.");
+            }  
+        } while (!salir);    
+                
+            }
+                
+        } 
+    
+
